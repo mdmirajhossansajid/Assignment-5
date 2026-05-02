c10c20dfc731e5c699d0c9faf355000cc478db2a
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-05-02 11:18:20 +0600

    log in section complete

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("sign-in-btn").addEventListener("click", function() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "admin" && password === "admin123") {
+        alert("Login successful!");
+        window.location.assign("home.html");
+    }
+    else {
+        alert("Invalid credentials. Please try again.");
+        return ;
+    }
+});
